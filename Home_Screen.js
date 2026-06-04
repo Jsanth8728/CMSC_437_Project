a4a16315c6644337ca191095d630a7f2952cd1f3
Jsanth8728 <awesomekid4312@gmail.com>
2026-06-04 11:28:35 -0400

    Updating based on lecture

---
+++ Home_Screen.js
@@ -0,0 +1,45 @@
+
+function HomePage(){
+    return(
+        <div class = "homepage" id = "home">
+            <span class="menu" id="menu">
+                <span>
+                    <a href="#">
+                        <img src="images/profilepic.png" alt="Profile" className="profile-pic" />
+                    </a>
+                </span>
+                <span>
+                    <a href="#">
+                        <img src="images/Clapboard.png" alt="Entertainment" className="clapboard-pic" />
+                    </a>
+                </span>
+                <span>
+                    <a href="#">
+                        <img src="images/AirplaneIcon.png" alt="Airplane" className="airplane-pic" />
+                    </a>
+                </span>
+                <span>
+                    <a href="#">
+                        <img src="images/ShoppingBag.png" alt="Shopping" className="shopping-pic" />
+                    </a>
+                </span>
+                <span>
+                    <a href="#">
+                        <img src="images/ForkKnife.png" alt="Food and Beverage" className="food-pic" />
+                    </a>
+                </span>
+                <span>
+                    <a href="#">
+                        <img src="images/Help.png" alt="Help" className="help-pic" />
+                    </a>
+                </span>
+            </span>
+            <div class = "topbar" id = "topbar">
+                <p>Hello World</p>
+            </div>
+        </div>
+    );
+}
+function TopBar(){
+    
+}
