bd324185540afcfa99befef4678f29750140c79b
Dennette Affinito <daffini1@umbc.edu>
2026-06-29 17:51:17 -0400

    Added login to homepage

--- Home_Screen.js
+++ Home_Screen.js
@@ -1,63 +1,85 @@
+function goToMediaPage() {
+    window.location.href = "Media.html";
+}
 
-function HomePage(){
-    return(
-        <div class = "homepage" id = "home">
-            <span class="menu" id="menu">
+function goToHomePage() {
+    window.location.href = "Home_Screen.html";
+}
+
+function HomePage() {
+    return (
+        <div className="homepage" id="home">
+            <span className="menu" id="menu">
                 <span>
                     <a href="#">
-                        <img 
-                        src="images/profilepic.png" 
-                        alt="Profile" 
-                        className="profile-pic" />
+                        <img
+                            src="images/profilepic.png"
+                            alt="Profile"
+                            className="profile-pic"
+                        />
                     </a>
                 </span>
+
+                <span>
+                    <img
+                        src="images/Clapboard.png"
+                        alt="Entertainment"
+                        className="clapboard-pic"
+                        onClick={goToMediaPage}
+                    />
+                </span>
+
                 <span>
                     <a href="#">
-                        <img src="images/Clapboard.png" 
-                        alt="Entertainment" 
-                        className="clapboard-pic" 
-                        onClick = {goToMediaPage}/>
+                        <img
+                            src="images/AirplaneIcon.png"
+                            alt="Airplane"
+                            className="airplane-pic"
+                        />
                     </a>
                 </span>
+
                 <span>
                     <a href="#">
-                        <img src="images/AirplaneIcon.png" 
-                        alt="Airplane" 
-                        className="airplane-pic" />
+                        <img
+                            src="images/ShoppingBag.png"
+                            alt="Shopping"
+                            className="shopping-pic"
+                        />
                     </a>
                 </span>
+
                 <span>
                     <a href="#">
-                        <img src="images/ShoppingBag.png" 
-                        alt="Shopping" 
-                        className="shopping-pic" />
+                        <img
+                            src="images/ForkKnife.png"
+                            alt="Food and Beverage"
+                            className="food-pic"
+                        />
                     </a>
                 </span>
+
                 <span>
                     <a href="#">
-                        <img src="images/ForkKnife.png" 
-                        alt="Food and Beverage" 
-                        className="food-pic" />
-                    </a>
-                </span>
-                <span>
-                    <a href="#">
-                        <img src="images/Help.png" 
-                        alt="Help" 
-                        className="help-pic" />
+                        <img
+                            src="images/Help.png"
+                            alt="Help"
+                            className="help-pic"
+                        />
                     </a>
                 </span>
             </span>
-            <div class = "topbar" id = "topbar">
-                <p>Hello World</p>
+
+            <div className="topbar">
+                <div className="media-banner">
+                    <div className="announcement">Announcements</div>
+
+                    <div className="eta">ETA</div>
+
+                    <div className="login">Login</div>
+
+                </div>
             </div>
         </div>
     );
-}
-function TopBar(){
-    
-}
-
-function goToMediaPage() {
-    window.location.href="Media.html"
 }
